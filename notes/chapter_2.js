6f3a6fdf1589c01dd575c040bf44676152eba0e3
Justin Billeci <justinbilleci15@gmail.com>
2024-03-04 13:25:31 -0500

    Update chapter_2.js

--- notes/chapter_2.js
+++ notes/chapter_2.js
@@ -1,9 +1,46 @@
-//const let var//
+// const let var
+let sentence, number_of_eggs
 
-const sentence = 'hi mom'
+sentence = 'hi mom' // able to use notes on the same line
 
-let number_of_eggs = 3
+number_of_eggs = 3
 
-number_of_eggs = 8 
+// sentence = 'hi dad'
 
-console.log(number_of_eggs)
+let total_groceries = number_of_eggs
+console.log(sentence)
+console.log(total_groceries)
+
+
+
+12312313123 // this is a number
+
+'hello world' // string of characters 
+
+undefined // absence of a value due to it not being assigned yet  
+
+null // absence of value, different than undefined
+
+//boolean = true/false
+true & false // these are booleans
+
+{} // an object/dictionary
+let couple = {
+    justin: 'incredibly handsome', // must be separated with comma
+    Katherine: 'isn\'t dumb', // can use \ to keep JS happy, can also use the opposite, for example " instead of '
+    number_of_eggs: 3
+} // object/dictionary
+
+let example_array = [1, 2, 3, 4] // comma then space it out, JS uses a 0 indexing system not a 1
+
+let is_true = true // statement doesn't make sense but is used to show that you can assign any data type value to a variable 
+
+
+function multiplyNumbers(value, secondValue) {
+    // output the value of is_true
+    console.log(value * secondValue)
+}
+
+multiplyNumbers(3, 5)
+multiplyNumbers(8, 10)
+multiplyNumbers(12, 1)
