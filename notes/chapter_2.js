2973360de04fdf53b3d5d84b5529212f33ae652b
Justin Billeci <justinbilleci15@gmail.com>
2024-03-04 19:47:41 -0500

    Update chapter_2.js

--- notes/chapter_2.js
+++ notes/chapter_2.js
@@ -31,16 +31,61 @@ let couple = {
     number_of_eggs: 3
 } // object/dictionary
 
-let example_array = [1, 2, 3, 4] // comma then space it out, JS uses a 0 indexing system not a 1
 
 let is_true = true // statement doesn't make sense but is used to show that you can assign any data type value to a variable 
 
 
-function multiplyNumbers(value, secondValue) {
-    // output the value of is_true
-    console.log(value * secondValue)
+function firstFunction(value, secondValue) {
+    // multiplies two numbers
+    console.log(value * secondValue) // * multiply , + add , / divide , % modulus/remainder , - subtract
 }
 
-multiplyNumbers(3, 5)
-multiplyNumbers(8, 10)
-multiplyNumbers(12, 1)
+let multiplyTwoNumbers = firstFunction
+
+multiplyTwoNumbers(3, 5)
+multiplyTwoNumbers(8, 10)
+multiplyTwoNumbers(12, 1)
+
+function secondFunction(value, secondValue) {
+    //checks if true or false
+    console.log(value == secondValue) // == boolean output, checks if equivalent,  === checks data type, != boolean output, checks if it is NOT equivalent
+}
+
+let isTrue = secondFunction
+
+isTrue(2, 3)
+isTrue(1, 1)
+isTrue(0, 9)
+
+// && AND, || OR, ! NOT  (0 is a false value)
+
+let condition1 = true
+let condition2 = false 
+
+console.log(condition1, condition2)
+
+//typeof defines what something is, function, array, object, etc.. 
+console.log(typeof isTrue)
+
+//if will read if statement is true
+if (condition2) {
+    console.log('hi mom')
+} else if (condition1 || condition2) {
+    console.log('one of the conditions was true')
+}
+else {
+    console.log('value above was clearly false if you\'re reading this LOL')
+}
+//else will read if first statement was false
+
+let example_array = [1, 2, 3, 4] // comma then space it out, JS uses a 0 indexing system not a 1
+
+let i = 0 
+let = length = example_array.length
+console.log(length)
+
+//executes the code if value is true, < less than, > greater than, = equal to, <= less than or equal to, >= greater than or equal to
+while ( i < length) {
+    console.log('value was true: ', i, example_array[i])
+    i++ // i++ increment by 1 every time, i-- decrease by one every time
+}
